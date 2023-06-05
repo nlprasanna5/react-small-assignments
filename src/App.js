2f6fe6d772b2201d9db4fe2780665bc1f9ecdcea
nlprasanna5 <lakshmi.76740@gmail.com>
2023-06-05 23:22:58 +0530

    created simple mini calculter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './App.module.css';
 
-function App() {
+const App = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleNum1Change = (e) => {
+    setNum1(e.target.value);
+  };
+
+  const handleNum2Change = (e) => {
+    setNum2(e.target.value);
+  };
+
+  const handleAddition = () => {
+    setResult(Number(num1) + Number(num2));
+  };
+
+  const handleSubtraction = () => {
+    setResult(Number(num1) - Number(num2));
+  };
+
+  const handleMultiplication = () => {
+    setResult(Number(num1) * Number(num2));
+  };
+
+  const handleDivision = () => {
+    setResult(Number(num1) / Number(num2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.calculator}>
+      <h1>Calculator</h1>
+      <input type="number" value={num1} onChange={handleNum1Change} className={styles.inputField} />
+      <input type="number" value={num2} onChange={handleNum2Change} className={styles.inputField} />
+      <br />
+      <button onClick={handleAddition} className={styles.button}>Addition</button>
+      <button onClick={handleSubtraction} className={styles.button}>Subtraction</button>
+      <button onClick={handleMultiplication} className={styles.button}>Multiplication</button>
+      <button onClick={handleDivision} className={styles.button}>Division</button>
+      <br />
+      <h2 className={styles.result}>Result: {result}</h2>
     </div>
   );
-}
+};
 
 export default App;
