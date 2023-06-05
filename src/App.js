427254ab06079b5e2d5008a4ddf4dd0dbc4a285f
nlprasanna5 <lakshmi.76740@gmail.com>
2023-06-06 00:03:12 +0530

    created simple student details form

--- src/App.js
+++ src/App.js
@@ -1,47 +1,102 @@
-import React, { useState } from 'react';
-import styles from './App.module.css';
+import React, { useState } from "react";
+import styles from "./App.module.css";
 
 const App = () => {
-  const [num1, setNum1] = useState('');
-  const [num2, setNum2] = useState('');
-  const [result, setResult] = useState('');
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [grade, setGrade] = useState("");
+  const [subject, setSubject] = useState("");
+  const [students, setStudents] = useState([]);
+  const [error, setError] = useState("");
 
-  const handleNum1Change = (e) => {
-    setNum1(e.target.value);
-  };
-
-  const handleNum2Change = (e) => {
-    setNum2(e.target.value);
-  };
-
-  const handleAddition = () => {
-    setResult(Number(num1) + Number(num2));
-  };
-
-  const handleSubtraction = () => {
-    setResult(Number(num1) - Number(num2));
-  };
-
-  const handleMultiplication = () => {
-    setResult(Number(num1) * Number(num2));
-  };
-
-  const handleDivision = () => {
-    setResult(Number(num1) / Number(num2));
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !age || !grade || !subject) {
+      setError("Please fill all input fields");
+      return;
+    }
+    const student = {
+      name: name,
+      age: age,
+      grade: grade,
+      subject: subject
+    };
+    setStudents([...students, student]);
+    setName("");
+    setAge("");
+    setGrade("");
+    setSubject("");
   };
 
   return (
-    <div className={styles.calculator}>
-      <h1>Calculator</h1>
-      <input type="number" value={num1} onChange={handleNum1Change} className={styles.inputField} />
-      <input type="number" value={num2} onChange={handleNum2Change} className={styles.inputField} />
-      <br />
-      <button onClick={handleAddition} className={styles.button}>Addition</button>
-      <button onClick={handleSubtraction} className={styles.button}>Subtraction</button>
-      <button onClick={handleMultiplication} className={styles.button}>Multiplication</button>
-      <button onClick={handleDivision} className={styles.button}>Division</button>
-      <br />
-      <h2 className={styles.result}>Result: {result}</h2>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Student Form</h2>
+      <p className={styles.error}>{error}</p>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Age:</label>
+          <input
+            type="text"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Grade:</label>
+          <input
+            type="text"
+            value={grade}
+            onChange={(e) => setGrade(e.target.value)}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label className={styles.label}>Subject:</label>
+          <input
+            type="text"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            className={styles.input}
+          />
+        </div>
+
+        <button type="submit" className={styles.button}>
+          Submit
+        </button>
+      </form>
+
+      {students.length > 0 && (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Grade</th>
+              <th>Subject</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student, index) => (
+              <tr key={index}>
+                <td>{student.name}</td>
+                <td>{student.age}</td>
+                <td>{student.grade}</td>
+                <td>{student.subject}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
