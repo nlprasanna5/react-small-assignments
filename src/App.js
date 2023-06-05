8f079b421f40fa2fad9297309f63255ffcadada5
nlprasanna5 <lakshmi.76740@gmail.com>
2023-06-06 00:32:34 +0530

    created simple form

--- src/App.js
+++ src/App.js
@@ -1,104 +1,94 @@
 import React, { useState } from "react";
-import styles from "./App.module.css";
+import "./App.css";
 
-const App = () => {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [grade, setGrade] = useState("");
-  const [subject, setSubject] = useState("");
-  const [students, setStudents] = useState([]);
-  const [error, setError] = useState("");
+function App() {
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    mobile: "",
+    password: ""
+  });
+  const [users, setUsers] = useState([]);
 
-  const handleSubmit = (e) => {
+  function changeHandler(e) {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [e.target.name]: e.target.value
+    }));
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
-    if (!name || !age || !grade || !subject) {
-      setError("Please fill all input fields");
-      return;
-    }
-    const student = {
-      name: name,
-      age: age,
-      grade: grade,
-      subject: subject
-    };
-    setStudents([...students, student]);
-    setName("");
-    setAge("");
-    setGrade("");
-    setSubject("");
-  };
+    setUsers((prevUsers) => [...prevUsers, formData]);
+    console.log(formData); // Display the current form data in the console
+    setFormData({
+      email: "",
+      username: "",
+      mobile: "",
+      password: ""
+    });
+  }
 
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Student Form</h2>
-      <p className={styles.error}>{error}</p>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <label className={styles.label}>Name:</label>
+    <div className="container">
+      <h1>Simple Form</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email:</label>
           <input
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className={styles.input}
+            name="email"
+            value={formData.email}
+            placeholder="Email"
+            onChange={changeHandler}
           />
         </div>
-        <div className={styles.formGroup}>
-          <label className={styles.label}>Age:</label>
+        <div className="form-group">
+          <label>Username:</label>
           <input
             type="text"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-            className={styles.input}
+            name="username"
+            value={formData.username}
+            placeholder="Username"
+            onChange={changeHandler}
           />
         </div>
-        <div className={styles.formGroup}>
-          <label className={styles.label}>Grade:</label>
+        <div className="form-group">
+          <label>Mobile:</label>
           <input
             type="text"
-            value={grade}
-            onChange={(e) => setGrade(e.target.value)}
-            className={styles.input}
+            name="mobile"
+            value={formData.mobile}
+            placeholder="Mobile"
+            onChange={changeHandler}
           />
         </div>
-        <div className={styles.formGroup}>
-          <label className={styles.label}>Subject:</label>
+        <div className="form-group">
+          <label>Password:</label>
           <input
-            type="text"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            className={styles.input}
+            type="password"
+            name="password"
+            value={formData.password}
+            placeholder="Password"
+            onChange={changeHandler}
           />
         </div>
-
-        <button type="submit" className={styles.button}>
-          Submit
-        </button>
+        <button type="submit">Submit</button>
       </form>
 
-      {students.length > 0 && (
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Grade</th>
-              <th>Subject</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map((student, index) => (
-              <tr key={index}>
-                <td>{student.name}</td>
-                <td>{student.age}</td>
-                <td>{student.grade}</td>
-                <td>{student.subject}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+      <div className="user-list">
+        <h2>User List:</h2>
+        {users.map((user, index) => (
+          <div key={index} className="user-item">
+            <p>Email: {user.email}</p>
+            <p>Username: {user.username}</p>
+            <p>Mobile: {user.mobile}</p>
+            <p>Password: {user.password}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-};
+}
 
 export default App;
